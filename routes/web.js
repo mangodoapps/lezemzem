f2984f30f90fa7be15c9f7533bd65f451838148e
muhammetrifat <turkmenrifat97@gmail.com>
2025-05-22 16:23:40 +0300

    domain gelene kadar recaptcha kapatıldı

--- routes/web.js
+++ routes/web.js
@@ -33,7 +33,7 @@ Route.get("/", SiteHomeController.getHome);
 
 Route.get("/about", AboutController.getAbout);
 Route.get("/contact", ContactController.getContact);
-Route.post("/contact", ContactController.postContact).middleware("site.contact.form");
+Route.post("/contact", ContactController.postContact);//.middleware("site.contact.form"); domain gelince açılacak
 
 Route.prefix("/industries").group(function (){
     // Route.get("/", IndustriesController.getIndustries);
